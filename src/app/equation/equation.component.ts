d64915737ca3d81ace22ebd590133ad96fa9dc6a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-17 22:44:27 +0530

    Create new formgroup with 3 form control and a method to generate random number between 0 and 9

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
-
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl(''),
+    }
+  );
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
   }
 
 }
