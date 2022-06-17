944e027ab0f73743e503292ba6827232a0bb9d4a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-17 23:13:10 +0530

    Extract custom validation logic as static method in MathValidators class

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { MathValidators } from '../math-validators';
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
@@ -11,15 +12,7 @@ export class EquationComponent implements OnInit {
       a: new FormControl(this.randomNumber()),
       b: new FormControl(this.randomNumber()),
       answer: new FormControl(''),
-    }, [
-      (form: AbstractControl) => {
-        const { a, b, answer} = form.value;
-        if (a + b === parseInt(answer)) {
-          return null;
-        }
-        return { addition: true };  
-      }
-    ]
+    }, [MathValidators.addition]
   );
   constructor() { }
 
